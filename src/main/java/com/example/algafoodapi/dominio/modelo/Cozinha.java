4a9b79628efd4bec235738d4d80b4847cef3d060
Carlos Roberto <carlosmedeiroslima@gmail.com>
2020-06-16 21:13:02 -0300

    Atualização do projeto

---
+++ src/main/java/com/example/algafoodapi/dominio/modelo/Cozinha.java
@@ -0,0 +1,9 @@
+package com.example.algafoodapi.dominio.modelo;
+/*
+ *  @criado em: 16/06/2020 - {20:17}
+ *  @projeto  : algafood-api
+ *  @autor    : roberto
+ */
+
+public class Cozinha {
+}
---
+++ src/main/java/com/example/algafoodapi/dominio/modelo/Restaurante.java
@@ -0,0 +1,8 @@
+package com.example.algafoodapi.dominio.modelo;/*
+ *  @criado em: 16/06/2020 - {20:30}
+ *  @projeto  : algafood-api
+ *  @autor    : roberto
+ */
+
+public class Restaurante {
+}
